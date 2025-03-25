a1528967727e88c2dcb79db9f98dfe47e321535d
vpinedo <74575048+vpinedo07@users.noreply.github.com>
2025-03-25 11:17:40 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("http://54.224.133.169/api-php-world/api/get_countries.php")
+        .then(response => response.json())
+        .then(data => {
+            const tableBody = document.getElementById("countriesTable");
+            data.forEach(country => {
+                const row = document.createElement("tr");
+                row.innerHTML = `
+                    <td>${country.code}</td>
+                    <td>${country.name}</td>
+                    <td>${country.continent}</td>
+                    <td>${country.region}</td>
+                    <td>${country.population}</td>
+                `;
+                tableBody.appendChild(row);
+            });
+        })
+        .catch(error => console.error("Error al obtener los datos:", error));
+});
